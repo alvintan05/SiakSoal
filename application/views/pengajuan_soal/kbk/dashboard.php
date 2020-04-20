e871b57b6b72775b5af735994ec935b0a772069d
maulidya26 <56064858+maulidya26@users.noreply.github.com>
2020-04-20 22:27:03 +0700

    dashboard kbk

--- application/views/pengajuan_soal/kbk/dashboard.php
+++ application/views/pengajuan_soal/kbk/dashboard.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title><?php echo $main['title']; ?></title> 
-  <?php $this->load->view('layouts/head') ?>
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title><?php echo $main['title']; ?></title>
+	<?php $this->load->view('layouts/head') ?>
+	<link rel="stylesheet" href="">
 </head>
 <body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
 	<div class="wrapper">
 
 		<!-- navbar -->
-			<?php $this->load->view('layouts/navbar/navbar_dosen') ?>
+			<?php $this->load->view('layouts/navbar/navbar_kbk')?>
 		<!-- END navbar -->
 		
 		<!-- sidebar -->
-			<?php $this->load->view('layouts/sidebar/sidebar_dosen')?>
+			<?php $this->load->view('layouts/sidebar/sidebar_kbk')?>
 		<!-- END sidebar -->
 
 		<!-- Content Wrapper. Contains page content -->
 		<div class="content-wrapper">
 			<!-- pages -->
-			<?php $this->load->view('pages/dosen/home') ?>
+			<?php $this->load->view('pages/kbk/dashboard') ?>
 			<!-- END pages -->
 		</div>
 		<!-- /.content-wrapper -->
-
+		
 		<!-- Control Sidebar -->
 		<aside class="control-sidebar control-sidebar-dark">
 			<!-- Control sidebar content goes here -->
@@ -32,12 +35,12 @@
 		<!-- footer -->
 		<?php $this->load->view('layouts/footer')?>
 		<!-- END footer -->
+
 	</div>
 	<!-- ./wrapper -->
 
-	<!-- javascrAAipt -->
+	<!-- javascript -->
 	<?php $this->load->view('layouts/javascript')?>
 	<!-- END javascript -->
-
 </body>
 </html>
