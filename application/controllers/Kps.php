452f5d9f398df39dc1b4964324899ece157775a3
KerangRebus12 <iqbalridho51@gmail.com>
2020-05-16 18:52:25 +0700

    Update Kps.php

--- application/controllers/Kps.php
+++ application/controllers/Kps.php
@@ -12,6 +12,6 @@ class Kps extends CI_Controller
 	{
 		$data['title'] = 'Home | KPS';
 		$data['pages'] = $this->load->view('pages/kps/bank_soal','',true);
-		$this->load->view('pengajuan_soal/kps/bank_soal.php');
+		$this->load->view('pengajuan_soal/kps/bank_soal.php', array('main'=>$data));
 	}
 }
