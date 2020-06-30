053f29891e6a08956afebe9273c7eb4aa5b246ca
Nuraminah1017 <62093126+Nuraminah1017@users.noreply.github.com>
2020-06-30 22:45:26 +0700

    Update Kbk.php

--- application/controllers/Kbk.php
+++ application/controllers/Kbk.php
@@ -27,6 +27,10 @@ class Kbk extends CI_Controller
 	function dashboard()
 	{
 		$data['title'] = 'Dashboard | KBK';
+		$id = array('kbk_nip'=>$this->session->kbk_nip);
+		$Daftarsoaluas = json_decode($this->curl->simple_get($this->API.'kbk/Daftarsoaluas', $id));
+		$data['Daftarsoaluas'] = $Daftarsoaluas->data;
+
 		$this->load->view('pengajuan_soal/kbk/dashboard.php', array('main'=>$data));
 	}
 
