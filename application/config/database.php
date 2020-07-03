b357fbb7130e5ebd5b168c05f7e421ae3891afb4
musyaffanq <50899323+musyaffanq@users.noreply.github.com>
2020-07-03 11:22:15 +0700

    .

--- application/config/database.php
+++ application/config/database.php
@@ -80,6 +80,11 @@ $db['default'] = array(
 	'password' => '',
 	'database' => '',
 	'dbdriver' => 'mysqli',
+	// 'hostname' => 'localhost',
+	// 'username' => 'openpg',
+	// 'password' => 'hahaha123',
+	// 'database' => 'TIK_DB',
+	// 'dbdriver' => 'postgre',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
 	'db_debug' => (ENVIRONMENT !== 'production'),
@@ -92,5 +97,6 @@ $db['default'] = array(
 	'compress' => FALSE,
 	'stricton' => FALSE,
 	'failover' => array(),
-	'save_queries' => TRUE
+	'save_queries' => TRUE,
+	// 'port'	=> 5432
 );
